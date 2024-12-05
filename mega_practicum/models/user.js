145bb7f6455959aadfad05e715664c3ddcbd6ca6
camh00 <113644669+camh00@users.noreply.github.com>
2024-12-04 23:10:47 -0700

    cleaned up mega

--- mega_practicum/models/user.js
+++ mega_practicum/models/user.js
@@ -1,27 +1,9 @@
-// Filename - model/User.js
-
-// const mongoose = require('mongoose')
-// const Schema = mongoose.Schema
-// const passportLocalMongoose = require('passport-local-mongoose');
-// var User = new Schema({
-//     id : mongoose.Schema.ObjectId,
-//     username: {
-//         type: String
-//     },
-//     password: {
-//         type: String
-//     }
-// })
-
-// User.plugin(passportLocalMongoose);
-
-// module.exports = mongoose.model('users', User)
-
 const mongoose = require('mongoose');
 const Schema = mongoose.Schema;
 const passportLocalMongoose = require('passport-local-mongoose');
 const bcrypt = require('bcrypt');
 
+// Todo: Add a schoo id to the user schema
 const UserSchema = new Schema({
   username: { type: String, required: true, unique: true },
   password: { type: String, required: true },
